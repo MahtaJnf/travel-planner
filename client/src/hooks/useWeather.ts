7fddfe42ad8cd777e3cec481886e515b1c6a4e52
MahtaJannatifar <jannatif@usc.edu>
2025-05-19 19:34:08 -0700

    prevent weather api call before search

--- client/src/hooks/useWeather.ts
+++ client/src/hooks/useWeather.ts
@@ -3,12 +3,13 @@ import axios from 'axios';
 
 export const useWeather = (city: string) => {
   return useQuery({
-    queryKey: ['weather'],
+    queryKey: ['weather', city],
     queryFn: async () => {
       const res = await axios.get(
         `http://localhost:3333/api/v1/weather?city=${city}`
       );
       return res.data;
     },
+    enabled: !!city,
   });
 };
