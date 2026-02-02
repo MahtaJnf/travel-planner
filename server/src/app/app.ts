31b781ed778b3b4acebd8ba8393e96dfea916708
MahtaJannatifar <jannatif@usc.edu>
2026-02-01 16:06:21 -0800

    remove debug endpoint

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -23,14 +23,6 @@ app.get('/api', (_req, res) => {
   res.json({ message: 'Server is up and running' });
 });
 
-// Debug endpoint to check environment variables
-app.get('/api/debug/env', (_req, res) => {
-  res.json({
-    nodeEnv: process.env.NODE_ENV,
-    hasUnsplashKey: !!process.env.UNSPLASH_ACCESS_KEY,
-    unsplashKeyLength: process.env.UNSPLASH_ACCESS_KEY?.length || 0,
-  });
-});
 app.use('/api/v1/weather', weatherRouter);
 app.use('/api/v1/country', countryRouter);
 app.use('/api/v1/', imageRouter);
